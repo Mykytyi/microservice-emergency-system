2bf57835d5c703367d69a2f92b9405e595d0915b
Cherevatyi Mykyta <nikita.cherevatyi@extern.a1.at>
2024-08-18 13:12:00 +0300

    Configured husky

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,9 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const one = 1;
-
 app.get('/', (req: Request, res: Response) => {
   res.send('My server');
 });
-
-if (port===3000) {
-  console.log(123);
-}
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
