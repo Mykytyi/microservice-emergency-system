b0f5427c5ca8495391b7c089bee9f8b46dcbde64
Cherevatyi Mykyta <nikita.cherevatyi@extern.a1.at>
2024-08-20 17:03:22 +0300

    CORS configs; TypeORM installed

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import 'reflect-metadata';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('My server');
+// Configure CORS to allow requests from certain domains
+const corsOptions = {
+  origin: 'http://localhost:3001', // allowed domains
+  methods: ['GET', 'POST'], // allowed methods
+  credentials: true // allows cookies (if necessary)
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.post('/login', (req: Request, res: Response) => {
+  res.json({ message: 'My server' });
 });
 
 app.listen(port, () => {
